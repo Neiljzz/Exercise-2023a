8f6a5c6797e29ab81d745206274d2d7e1765e486
Neiljzz <neiljzzzzzz@gmail.com>
2023-05-03 20:25:24 -0600

    Chapter 2 updated

--- Chapter 2/demo01.py
+++
@@ -1,5 +0,0 @@
-names = ['Neil', 'Dori', 'Jenny', 'Brain', 'Mickey']
-for name in names:
-    print("Hello " + name)
-
-print(names)
--- Chapter 2/demo02.py
+++
@@ -1,6 +0,0 @@
-names = "Alan, Bruce, Carlos, David, Emma"
-name_list = names.split(",")
-# print(name_list)
-for name in name_list:
-    name2 = name.strip()
-    print('hello, %s'%name2)
